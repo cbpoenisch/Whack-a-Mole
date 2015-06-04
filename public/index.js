3b347dc310e55606af5cd67bc38729cea376e01e
Crystal Poenisch <crystalpoenisch@Crystal-Poenischs-MacBook-Pro.local>
2015-06-04 18:45:08 -0500

    initial commit

---
+++ public/index.js
@@ -0,0 +1,42 @@
+
+
+<script type="text/javascript" src="jquery-1.11.3.js"></script>	
+
+<script type="text/javascript">
+	(function() {
+
+		use "strict";
+
+		var game = {
+
+			moles: $('.mole'), 
+			score: 0.
+			level: 1,
+
+		// initialize the game
+			init: function() {
+				this.startTime();
+			},
+
+		// select random hole using math.random
+			getRandomHole: function() {
+
+			}, 
+
+			userClickEvent: function() {
+
+			}
+
+			addScore: function() {
+				this.score++;
+				// change html, make sure to write html for score
+			},
+
+			levelUp: function() {
+
+			},
+
+		}
+
+	})();
+</script>
